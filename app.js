ef094e1766140eb56a540c57d5b7426baaf4f3e7
rosemarym2 <rosemarymay91@gmail.com>
2021-11-02 11:13:04 +0000

    changes v1.8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const displayeventKey = document.getElementsByClassName("eventKey");
 const displayeventCode = document.getElementsByClassName("eventCode");
 const displayeventWhich = document.getElementsByClassName("eventWhich");
 
-window.addEventListener("keydown", (e) =>{
+window.addEventListener(`keydown`, (e) =>{
     displayeventKey.textContent = `${e.Key}`;
     displayeventCode.textContent = `${e.Code}`;
     displayeventCode.textContent = `${e.Which}`;
